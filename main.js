c8e9b39864bafc6abc1bac7d99d5ba448f47532e
marko <star_tux@yahoo.com>
2019-05-14 19:31:03 +0200

    Dodato dugme za lokaciju korisnika

--- main.js
+++ main.js
@@ -51,6 +51,43 @@ function initMap() {
       map: map
     });
   };
+
+  let object = document.getElementById("my_location");
+  object.onclick = function(){    
+    // HTML5 geolocation.
+    infoWindow = new google.maps.InfoWindow;   
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+
+        var lokacija = {lat: pos.lat, lng: pos.lng};
+        var marker = new google.maps.Marker({position: lokacija, map: map});
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    }
+  }
+
+
 }
 
   // Get current location
