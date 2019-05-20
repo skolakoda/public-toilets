455df4677d93f02f878caf4e1b52e45751c40c65
ivangochanin <ivangochanin@gmail.com>
2019-05-20 17:26:11 +0100

    add fetch locations

--- main.js
+++ main.js
@@ -1,105 +1,81 @@
+/* global google */
 let map;
+// eslint-disable-next-line no-unused-vars
 let marker;
-
-
+// eslint-disable-next-line no-unused-vars
 function initMap() {
-  map = new google.maps.Map(
-    document.getElementById('map'), {
-      center: new google.maps.LatLng(44.817197, 20.437273),
-      zoom: 14
-    });
+  map = new google.maps.Map(document.querySelector('#map'), {
+    center: new google.maps.LatLng(44.817197, 20.437273),
+    zoom: 12
+  });
 
-  let iconBase =
-    'http://maps.google.com/mapfiles/kml/shapes/';
+  const iconBase1 = 'http://maps.google.com/mapfiles/kml/pal2/';
+  const iconBase2 = 'http://maps.google.com/mapfiles/kml/pal3/';
 
-  let icons = {
+  const icons = {
+    default: {
+      icon: `${iconBase1}icon10.png`
+    },
     info: {
-      icon: iconBase + 'info.png'
+      icon: `${iconBase2}icon44.png`
     },
-    man: {
-      icon: iconBase + 'man.png'
+    restaurants: {
+      icon: `${iconBase1}icon32.png`
     },
-    lodging: {
-      icon: iconBase + 'lodging.png'
+    radiation: {
+      icon: `${iconBase2}icon47.png`
     },
-    home: {
-      icon: iconBase + 'homegardenbusiness.png'
-    },
-    horse: {
-      icon: iconBase + 'horsebackriding.png'
+    market: {
+      icon: `${iconBase2}icon26.png`
     }
   };
 
-  let places = [{
-      position: new google.maps.LatLng(44.8116093, 20.4710836),
-      type: 'home'
-    },
-    {
-      position: new google.maps.LatLng(44.8482649, 20.3844028),
-      type: 'home'
-    },
-    {
-      position: new google.maps.LatLng(44.8141699, 20.4595646),
-      type: 'info'
-    },
-    {
-      position: new google.maps.LatLng(44.8115567, 20.4656869),
-      type: 'horse'
-    }
-  ];
-
-  // Create markers.
-  for (let i = 0; i < places.length; i++) {
-    marker = new google.maps.Marker({
-      position: places[i].position,
-      icon: icons[places[i].type].icon,
-      map: map
+  function createMarkers(places) {
+    // Create markers
+    places.forEach(i => {
+      marker = new google.maps.Marker({
+        position: i.position,
+        icon: icons[i.type].icon,
+        map
+      });
     });
+  }
+  function displayLocations(spomenici) {
+    const places = [];
+    spomenici.forEach(spomenik => {
+      const { lat } = spomenik.lokacija;
+      const { lon } = spomenik.lokacija;
+      const newLocation = {
+        position: new google.maps.LatLng(lat, lon),
+        type: 'radiation'
+      };
+      places.push(newLocation);
+    });
+    createMarkers(places);
+  }
+
+  fetch('https://spomenici-api.herokuapp.com/spomenici')
+    .then(response => response.json())
+    .then(data => {
+      displayLocations(data);
+    });
+
+  const myLocation = document.querySelector('#my_location');
+  myLocation.onclick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        map.setCenter(pos);
+
+        marker = new google.maps.Marker({
+          position: pos,
+          map
+        });
+      });
+    }
   };
-};
-
-
-
- let object = document.getElementById("my_location");
- object.onclick = function () {
-   // HTML5 geolocation.
-   infoWindow = new google.maps.InfoWindow;
-   if (navigator.geolocation) {
-     navigator.geolocation.getCurrentPosition(function (position) {
-       var pos = {
-         lat: position.coords.latitude,
-         lng: position.coords.longitude
-       };
-
-       infoWindow.setPosition(pos);
-       //infoWindow.setContent('Location found.');
-       // infoWindow.open(map);
-       map.setCenter(pos);
-
-       var lokacija = {
-         lat: pos.lat,
-         lng: pos.lng
-       };
-       var marker = new google.maps.Marker({
-         position: lokacija,
-         map: map
-       });
-     }, function () {
-       handleLocationError(true, infoWindow, map.getCenter());
-     });
-   } else {
-     // Browser doesn't support Geolocation
-     handleLocationError(false, infoWindow, map.getCenter());
-   }
-
-   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-     infoWindow.setPosition(pos);
-     infoWindow.setContent(browserHasGeolocation ?
-       'Error: The Geolocation service failed.' :
-       'Error: Your browser doesn\'t support geolocation.');
-     infoWindow.open(map);
-   }
- }
-
-
-
+}
